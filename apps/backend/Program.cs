f510a35be550617f0190a25997341376a6e5a4c4
Daniel Kreitsch <hello@danielkreitsch.com>
2025-04-07 11:45:19 +0200

    Add core library to backend and remove unnecessary code

--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -1,17 +1,7 @@
+using VoxelCommand.Core;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
@@ -35,7 +25,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
